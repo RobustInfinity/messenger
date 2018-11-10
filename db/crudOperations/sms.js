d0d379c03cd2449a46ac49e9cead9da4a64e3893
mishraprince0786@gmail.com <mishraprince0786@gmail.com>
2018-11-10 16:03:49 +0530

    v_2.0

--- db/crudOperations/sms.js
+++ db/crudOperations/sms.js
@@ -1,33 +1,33 @@
 
 const SMS = require('../models/SMS')
+const {prepareSMS,sendSMS} = require('../../config')
+
+
 
 const dbOperations = {
     createSMS : function (messageObj, callback){
-        var data = {}
-        data['messageId'] = messageObj.sid
-        data['account_sid'] = messageObj.accountSid
-        data['to'] = messageObj.to  //saving without country code
-        data['from'] = messageObj.from
-        data['body'] = messageObj.body
-        data['status'] = 'Sent'
-        data['direction'] = messageObj.direction
-        data['service'] = messageObj.service
-        data['type'] = messageObj.type || 'SMS'
-        data['date_created'] = messageObj.dateCreated || Date.now()
-        data['date_sent'] = messageObj.dateSent || Date.now()
-        var sms = new SMS(data)
-        sms.save(function(error, result){
+    
+        var messages = []
+        messageObj.to.forEach((value)=>{
+            messages.push(prepareSMS(value,messageObj.text,messageObj.type,messageObj.service))
+        })
+        messages.forEach((message)=>{
+            sendSMS(message)
+        })
+
+        SMS.insertMany(messages,(error, result)=>{
+            
             if(error){
                 callback(error, null)
             }else{
                 if(!result){
                     callback(null, null)
                 }else{
-                    console.log(result)
                     callback(null, result)
                 }
             }
         })
+       
     },
     getSMS : function(callback){
         SMS.find((error, result)=>{
--- routes/sms.js
+++ routes/sms.js
@@ -2,99 +2,114 @@ var express = require('express');
 var router = express.Router();
 
 const {_} = require('../config/utils')
-const {sendSMS, sendOTP} = require('../config')
 const {SERVICES, TYPE} = require('../config/config')
 
 
 const validateSMS = function(object){
 
   const errors = {}
-  console.log(object.to.length)
-  if(_.isEmpty(object.to) || object.to.length !== 10){
-      errors.to = 'Not a Valid Contact Number'
-  }
-
-  if(_.isEmpty(object.text) && object.type !== TYPE.OTP){
-      errors.text = 'Message Body cannot be empty'
-  }
-
-  if(_.isEmpty(object.type)){
-      errors.type = 'Message type cannot be empty'
-  }else{
-    var isExist = false;
-    Object.keys(TYPE).forEach(key=>{
-        if(TYPE[key] === object.type){
-            isExist = true
+    object.to.forEach((value, index)=>{
+        if(_.isEmpty(value) || value.split(' ').join('').length !== 10){
+            // console.log(value.length)
+            errors.to[index] = 'Not a Valid Contact Number'
         }
     })
-    if(!isExist){
-        errors.service = 'Invalid Type'
+    
+
+    if(_.isEmpty(object.text) && object.type !== TYPE.OTP){
+        errors.text = 'Message Body cannot be empty'
     }
-}
-
-
   
-
-  if(_.isEmpty(object.service)){
-      object.service = SERVICES.MSG91
-  }else{
+    if(_.isEmpty(object.type)){
+        errors.type = 'Message type cannot be empty'
+    }else{
       var isExist = false;
-      Object.keys(SERVICES).forEach(key=>{
-          if(SERVICES[key] === object.service){
+      Object.keys(TYPE).forEach(key=>{
+          if(TYPE[key] === object.type){
               isExist = true
           }
       })
       if(!isExist){
-          errors.service = 'Invalid Service'
+          errors.type = 'Invalid Type'
       }
   }
-
-  return {
-      isValid : _.isEmpty(errors),
-      errors : errors
-  }
+  
+    if(_.isEmpty(object.service)){
+        object.service = SERVICES.MSG91
+    }else{
+        var isExist = false;
+        Object.keys(SERVICES).forEach(key=>{
+            if(SERVICES[key] === object.service){
+                isExist = true
+            }
+        })
+        if(!isExist){
+            errors.service = 'Invalid Service'
+        }
+    }
+  
+    return {
+        isValid : _.isEmpty(errors),
+        errors : errors
+    }
 }
 
 
 
 router.post('/send-sms', function(request, response) {
-      var body = _.pick(request.body,['text','to','service','type'])
-      console.log(body)
-      var {isValid, errors} = validateSMS(body)
-      body['body'] = body.text
-      if(isValid){
-        if(body.type !== TYPE.OTP){
-          sendSMS(body,(error, result)=>{
-            if(error){
-              console.log(error)
-              response.send({error,message : "Error Occured"})
-            }else{
-              if(!result){
-                response.send({result : null,message : 'Nothing happened'})
-              }else{
-                console.log(result)
-                response.status(200).send({result,message : 'Message sent successfully'})
-              }
-            }
-          })
-        }else{
-          sendOTP(body,(error, result)=>{
-            if(error){
-              console.log(error)
-              response.send({error,message : "Error Occured"})
-            }else{
-              if(!result){
-                response.send({result : null,message : 'Nothing happened'})
-              }else{
-                console.log(result)
-                response.status(200).send({result,message : 'OTP Sent on ' + body.to})
-              }
-            }
-          })
-        }
-      }else{
-        response.status(500).send({errors,message : 'Invalid Inputs'})
-      }
+       //"to" : ["an array of phone numbers"]
+       var body = _.pick(request.body,['text','to','service','type'])
+      //  console.log(body)
+       var {isValid, errors} = validateSMS(body)
+       body.to.forEach((value, index)=>{
+           body.to[index] = value.split(' ').join('')
+       })
+       
+       if(isValid){
+       //   if(body.type !== TYPE.OTP){
+           var SmsDbOperations = require('../db/crudOperations/sms')
+           SmsDbOperations.createSMS(body,(error, result)=>{
+               if(error){
+                    response.json({"message":"Some Error Occured Try again Later!","code":500,"success":false});
+               }else{
+                   if(!result){
+                       response.json({"message":"Unable To send","code":400,"success":false});
+                   }else{
+                       response.json({"message":"Successfully sent ","code":200,"success":true});
+                   }
+               }
+           })
+           // sendSMS(body,(error, result)=>{
+           //   if(error){
+           //     console.log(error)
+           //     response.send({error,message : "Error Occured"})
+           //   }else{
+           //     if(!result){
+           //       response.send({result : null,message : 'Nothing happened'})
+           //     }else{
+           //       console.log(result)
+           //       response.status(200).send({result,message : 'Message sent successfully'})
+           //     }
+           //   }
+           // })
+       //   }else{
+       //     sendOTP(body,(error, result)=>{
+       //       if(error){
+       //         console.log(error)
+       //         response.send({error,message : "Error Occured"})
+       //       }else{
+       //         if(!result){
+       //           response.send({result : null,message : 'Nothing happened'})
+       //         }else{
+       //           console.log(result)
+       //           response.status(200).send({result,message : 'OTP Sent on ' + body.to})
+       //         }
+       //       }
+       //     })
+       //   }
+       }else{
+         response.json({"message":"Invalid parameters","code":400,"success":false,"errors":errors});
+       }
 });
 
 
